3bbd98e6fbb927595ec8a7c54d6a80891f4ad612
prashantdelhi1995 <00udemy00@gmail.com>
2024-01-24 08:13:40 +0530

    Add existing file

---
+++ sharpner/getElementsByClassName Method/index.js
@@ -0,0 +1,8 @@
+// Write your code below:
+let fruit1= document.getElementsByClassName("fruit")
+fruit1[2].style.backgroundColor="yellow"
+for (let i=0;i<fruit1.length;i++){
+  fruit1[i].style.foreWeight="bold";
+  
+}
+//fruit1[0].style.foreWeight="bold";
