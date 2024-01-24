f6f9a099776e6140932ee4aef70a8d0347c5c8dc
prashantdelhi1995 <00udemy00@gmail.com>
2024-01-24 08:04:55 +0530

    Add existing file

---
+++ sharpner/getElementById Method/index.js
@@ -0,0 +1,12 @@
+// Write your code below:
+const header=document.getElementById('main-heading')
+
+header.textContent="Fruit World"
+header.style.color="orange"
+const background=document.getElementById('header')
+background.style.background="green"
+background.style.borderBottom="orange"
+const header1=document.getElementById('basket-heading')
+header1.style.color="green"
+const thanks=document.getElementById('thanks')
+thanks.innerHTML="<p>Please visit us again</p>"
