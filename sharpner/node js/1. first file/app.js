c724ad4c157f8ecb470d785a1f5042897619828e
prashantdelhi1995 <00udemy00@gmail.com>
2024-02-25 15:15:25 +0530

    updated something sharpner task 7 node js

--- sharpner/node js/1. first file/app.js
+++ sharpner/node js/1. first file/app.js
@@ -1,5 +1,33 @@
-const http=require("http")
-const server=http.createServer((req,res)=>{
-    console.log(req);
-})
-server.listen(3000)
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    const url = req.url;
+    if (url == "/home") {
+        res.write("<html>");
+        res.write("<head><title>Welcome home</title></head>");
+        res.write("<body>Welcome to home page</body>");
+        res.write("</html>");
+        res.end();
+    } else if (url == "/about") {
+        res.write("<html>");
+        res.write("<head><title>About</title></head>");
+        res.write("<body>Welcome to About Us page</body>");
+        res.write("</html>");
+        res.end();
+    } else if (url == "/node") {
+        res.write("<html>");
+        res.write("<head><title>Node</title></head>");
+        res.write("<body>Welcome to my Node.js project</body>");
+        res.write("</html>");
+        res.end();
+    } else {
+        res.write("<html>");
+        res.write("<head><title>This is my first page</title></head>");
+        res.write("<body>Hello from Node.js server</body>");
+        res.write("</html>");
+        res.end();
+    }
+});
+
+server.listen(3000);
